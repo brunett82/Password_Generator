9aa5324c9750b32d0c4a72952faedc923cb1e5a1
Rob Brunett <rb3@Robs-Air.lan>
2021-04-11 17:18:31 -0400

    Length of password function added

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Variables
+var number = '0123456789';
+var special = '~`!@#$%^&*()-_=+[]{},<>?/|*.'
+var lowerCase = 'abcdefghijklmnopqrstuvwxyz'
+var upperCase = lowerCase.toUpperCase();
+
+//Prompt functions
+/*When prompted for desired length of password user chooses length >8 but <128*/
+function lengthOfPwd(){
+  length = prompt('How many characters in your passowrd? (min = 8, max = 128)');
+    if (length>128){
+      alert('Password too long. (min = 8, max = 128)');
+      lengthOfPwd();
+    }
+    else if (length<8){
+      alert('Password too short. (min = 8, max = 128)')
+      lengthOfPwd();
+    }
+    return length;
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
